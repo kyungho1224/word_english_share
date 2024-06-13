b37070091ab770b10b7c156f34c779bb9bf5ac6d
kyungho1224 <dev.kyungho.kim@gmail.com>
2024-06-13 23:49:44 +0900

    init project

---
+++ src/main/java/kr/kro/teamstudy/wordenglishshare/WordEnglishShareApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.teamstudy.wordenglishshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordEnglishShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordEnglishShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/kro/teamstudy/wordenglishshare/WordEnglishShareApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kro.teamstudy.wordenglishshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordEnglishShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
